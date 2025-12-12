b9157e30bc736327f6e67e66230a1f217ab1d061
GSSS-Mirzewala <gsssmirzewala1954@gmail.com>
2025-12-12 14:41:30 +0530

    NEW UPDATE 02:40 PM (12th Dec. 2025)
    Removed Unwanted Password Requirements.

--- validators/PasswordValidator.js
+++ validators/PasswordValidator.js
@@ -8,14 +8,6 @@ const PasswordValidator = [
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long")
     .trim()
-    .matches(/[a-z]/)
-    .withMessage("Password must contain at least 1 lowercase character.")
-    .matches(/[A-Z]/)
-    .withMessage("Password must contain at least 1 Uppercase character.")
-    .matches(/[0-9]/)
-    .withMessage("Password must contain at least 1 Numeric character.")
-    .matches(/[!@#$%^&*(),.?":{}|<>]/)
-    .withMessage("Password must contain at least 1 Special character."),
 ];
 
 export default PasswordValidator;
