f658775cc73c610f282cbacd549837040dcad2af
= <gsssmirzewala1954@gmail.com>
2026-02-02 22:07:01 +0530

    NEW FIX 10:06 PM (02nd Feb. 2026)
    Just corrected the Errors.

--- middlewares/JWT.js
+++ middlewares/JWT.js
@@ -6,11 +6,11 @@ import ServerError from "../utils/ServerErrors.js";
 
 export function protect(req, res, next) {
   if (!req.cookies.AuthToken) {
-    return next(new ServerError("You are Not Loggedin!", 401));
+    return next(new ServerError("NOT_LOGGED_IN", 401));
   } else {
     const decoded = jwt.verify(req.cookies.AuthToken, process.env.JWT_SECRET);
     if (!decoded) {
-      return next(new ServerError("You are Not Loggedin!", 401));
+      return next(new ServerError("NOT_LOGGED_IN", 401));
     } else {
       next();
     }
@@ -19,7 +19,7 @@ export function protect(req, res, next) {
 
 export function redirect(req, res, next) {
   if (req.cookies.AuthToken) {
-    return next(new ServerError("Cannot access this Page!", 401));
+    return next(new ServerError("CANNOT_ACCESS_PAGE!", 401));
   } else {
     next();
   }
